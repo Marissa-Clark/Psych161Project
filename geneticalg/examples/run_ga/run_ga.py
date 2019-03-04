531ee398c147658e87884c253f80c0511a3d0666
Marissa-Clark <marissadclark@gmail.com>
2019-03-04 15:58:18 -0500

    Added mutation function

--- geneticalg/examples/run_ga/run_ga.py
+++ geneticalg/examples/run_ga/run_ga.py
@@ -1,15 +1,14 @@
 import pandas as pd
-from geneticalg import gen_chromosome, calc_duration, calc_fitness
-
+from geneticalg import gen_chromosome, calc_fitness_all, mutate
 
 video_data = pd.read_csv("./data/video_ts_data.csv", index_col=None)
-
 video_lengths = pd.read_csv("./data/video_lengths.csv").iloc[:, 0].values
-video_list = pd.read_csv("./data/video_list.csv", index_col=None).iloc[:, 0].values
+video_list = pd.read_csv("./data/video_list.csv",
+                         index_col=None).iloc[:, 0].values
 
 # Define Variables
 max_seconds = 120 * 60
-numAttempts = 10
+max_attempts = 10
 numGenerations = 20
 popSize = 20
 
@@ -19,5 +18,6 @@ chromosomes = []
 for i in range(popSize):
     chromosomes.append(gen_chromosome(max_seconds, video_list, video_lengths))
 
+calc_fitness_all(chromosomes)
 
-calc_fitness(chromosomes[0])
+mutate(chromosomes[0], max_attempts, video_lengths, max_seconds)
--- geneticalg/geneticalg.py
+++ geneticalg/geneticalg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-
-
 import numpy as np
+from numpy import linalg
+import random
 
 
 def gen_chromosome(max_seconds, video_list, video_lengths):
@@ -34,28 +34,107 @@ def gen_chromosome(max_seconds, video_list, video_lengths):
 
 def calc_duration(chromosome, video_lengths):
     """Calculates the duration of the set of videos when combined
-
     Parameters
     ----------
     chromosome : np.ndarray
         Dummy-coded chromosome of videos
     video_lengths : np.ndarray
-        Lenghs (in seconds) of each video
-
+        Lenghts (in seconds) of each video
     Returns
     -------
     int
         Length of set of videos
-
     """
     return sum(chromosome*video_lengths)
 
 
-def calc_fitness(chromsome, video_data):
+def calc_fitness_individual(chromosome, video_list, video_data):
+    """Calculate the "fitness" of an individual chromosome
+    Fitness is the determinant of the covariance matrix of the ts data for each
+    emotion
 
-    # If calculating fitness for one chromosome
-    if type(chromsome) == np.ndarray:
-        keep_videos = list(set(video_data*chromsome) - {0})
-        video_mat = video_data.loc[video_data].isin(keep_videos)
-        video_mat = video_mat.reset_index(drop=True).drop('video', axis=1)
-        return video_mat
+    Parameters
+    ----------
+    chromosome : np.ndarray
+        Dummy-coded chromosome of videos
+    video_list : np.ndarray
+        List of all video titles (in this case number identifiers)
+    video_data : pd dataframe
+        Dataframe of Emotion by Time w/ video as a column
+
+    Returns
+    -------
+    float
+        Determinant of the covariance matrix of all emotions by time
+
+    """
+    keep_videos = list(set(video_list*chromosome)-{0})
+    video_mat = video_data.loc[video_data['video'].isin(keep_videos)]
+    video_mat = video_mat.reset_index(drop=True).drop('video', axis=1)
+    video_cov = video_mat.cov()
+    return linalg.det(video_cov)
+
+
+def calc_fitness_all(chromosomes, video_list, video_data):
+    """Calculates fitness for all chromosomes
+
+    Parameters
+    ----------
+    chromosomes : np.ndarrray
+        List of chromosomes
+    video_list : np.ndarray
+        List of all video titles (in this case number identifiers)
+    video_data : pd dataframe
+        Dataframe of Emotion by Time w/ video as a column
+
+    Returns
+    -------
+    list
+        Determinant of the covariance matrix of all emotions by time
+
+    """
+    fitness = []
+    for chromosome in chromosomes:
+        fitness.append(calc_fitness_individual(chromosome, video_list,
+                       video_data))
+    return fitness
+
+
+def mutate(chromosome, max_attempts, video_lengths, max_seconds):
+    """Point mutation by removing one "gene" and adding another
+
+    Parameters
+    ----------
+    chromosome : np.ndarray
+        Dummy-coded chromosome of videos
+    max_attempts : int
+        Maximum number of tries to mutate (it may be possible theres no combo
+        that fits our criteria (< max_seconds))
+    video_lengths : np.ndarray
+        Lengths (in seconds) of each video
+    max_seconds : int
+        Maximum number of seconds allowed for all videos
+
+    Returns
+    -------
+    list
+        New chromosome with mutation
+
+    """
+    for attempt in range(max_attempts):
+        mutated = chromosome.copy()
+        # Pick a random point to mutated - copied from stack_overflow,
+        # find better way to write out
+        mutation_pt_rm = random.choice([i for i,
+                                        x in enumerate(chromosome) if x == 1])
+        mutation_pt_add = random.choice([i for i,
+                                         x in enumerate(chromosome) if x == 0])
+        mutated[mutation_pt_rm] = 0
+        mutated[mutation_pt_add] = 1
+
+        if (calc_duration(mutated, video_lengths) < max_seconds) and (
+        calc_duration(mutated, video_lengths) !=
+        calc_duration(chromosome, video_lengths)):
+            return mutated
+
+    return gen_chromosome()
